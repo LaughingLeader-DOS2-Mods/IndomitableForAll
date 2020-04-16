d30ca1f0dd4457392233dea2a38847e15c2c0ad9
LaughingLeader <laughingleader@gmail.com>
2020-04-15 23:59:34 -0500

    v43 update

---
+++ Mods/IndomitableForAll_7f5124c0-da04-4ce1-aa73-e22ed00bb711/Story/RawFiles/Lua/BootstrapClient.lua
@@ -0,0 +1,25 @@
+local function ModuleLoading_AddIndomatible()
+	Ext.Print("===================================================================")
+	Ext.Print("[IndomitableForAll:Bootstrap.lua] Adding Indomitable to all Character stats.")
+	local totalOverrides = 0
+	for i,stat in pairs(Ext.GetStatEntries("Character")) do
+		if not player_stats[stat] then
+			local talents = Ext.StatGetAttribute(stat, "Talents")
+			if talents == nil or talents == "" then
+				Ext.StatSetAttribute(stat, "Talents", "Indomitable")
+				totalOverrides = totalOverrides + 1
+			else
+				if talents:find("Indomitable", 1, true) ~= nil then
+					Ext.Print("[IndomitableForAll:Bootstrap.lua] Stat ("..stat..") already has Indomitable ("..talents.."). Skipping.")
+				else
+					Ext.StatSetAttribute(stat, "Talents", talents..";Indomitable")
+					totalOverrides = totalOverrides + 1
+				end
+			end
+		end
+	end
+	Ext.Print("[IndomitableForAll:Bootstrap.lua] Added the Indomitable talent to "..tostring(totalOverrides).." Character stats.")
+	Ext.Print("===================================================================")
+end
+
+--Ext.RegisterListener("ModuleLoading", ModuleLoading_AddIndomatible)
